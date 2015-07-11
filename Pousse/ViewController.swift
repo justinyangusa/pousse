bb5c5a87b742038857e85bbf2b5786b35419acf5
Justin Yang <justin@y4ng.com>
2015-07-10 21:35:51 -0400

    Initial commit from local git

---
+++ Pousse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pousse
+//
+//  Created by Justin Yang on 7/10/15.
+//  Copyright © 2015 Justin Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
